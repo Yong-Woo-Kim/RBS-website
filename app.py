15f11d277ff54af95196d31447713125b8194638
Yong-Woo-Kim <120232234+Yong-Woo-Kim@users.noreply.github.com>
2023-03-21 06:25:28 +0000

    insert page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import database as db
 
 app = Flask(__name__)
@@ -11,16 +11,21 @@ def hello_rbs():
   s_name = db.load_student_name()
   Contents = [{'title': 'Teacher', 'display': t_name},
                  {'title': 'Student', 'display': s_name}]
-  return render_template('home.html', contents=Contents, school='RBS')
-
+  return render_template('home.html', contents=Contents, title='RBS')
 
 @app.route('/Teacher')
 def display_teacher():
-  return render_template('detail-info.html', contents='Teachers' )
+  return render_template('detail-info.html', title='Teachers' )
   
 @app.route('/Student')
 def display_student():
-  return render_template('detail-info.html', contents='Students' )
+  return render_template('detail-info.html', title='Students' )
+
+@app.route('/Teachers/apply', methods=['POST'])
+def apply_teacher():
+  data = request.form
+  db.add_teacher(data)
+  return render_template('application_submitted.html', application=data)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
--- database.py
+++ database.py
@@ -2,11 +2,13 @@ import sqlite3
 
 conn = sqlite3.connect('rbs.db', check_same_thread=False)
 c = conn.cursor()
+id = 0
+
 
 def make_list(dic):
   result = ''
   i = 0
-  
+
   for k, v in dic:
     i += 1
     if i is len(dic):
@@ -15,11 +17,13 @@ def make_list(dic):
       result = result + v + ','
   return result
 
+
 def load_teacher_name():
   result = c.execute('select "display", name from teacher')
   t_name = result.fetchall()
   result = make_list(t_name)
   return result
+
 
 def load_student_name():
   result = c.execute('select "display", name from student')
@@ -27,8 +31,18 @@ def load_student_name():
   result = make_list(s_name)
   return result
 
+
 def load_teacher(id):
-  result = c.execute('select * from teacher where id = ?',(id)).fetchall()
-  print(type(result))
+  result = c.execute('select * from teacher where id = ?', (id)).fetchall()
+  print(result)
+
+
+def add_teacher(data):
+  c.execute(
+    'INSERT INTO teacher (id, name, birthday, gender, mobile, address) VALUES (?,?,?,?,?,?)',
+    ('10', data['name'], data['birthday'], data['gender'], data['mobile'],
+     data['address']))
+  conn.commit()
+
 
 load_teacher('1')
