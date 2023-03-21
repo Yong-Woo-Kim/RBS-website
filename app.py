e229869b4ebd6062e0b78b8c6bb28d4b250dbbc7
Yong-Woo-Kim <120232234+Yong-Woo-Kim@users.noreply.github.com>
2023-03-21 01:49:31 +0000

    change html page

--- app.py
+++ app.py
@@ -1,39 +1,26 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template
 import database as db
 
 app = Flask(__name__)
-  
+
+
+      
 @app.route('/')
 def hello_rbs():
-  t_result = ''
-  i = 0
   t_name = db.load_teacher_name()
-  for k, v in t_name:
-    i += 1
-    if i is len(t_name):
-      t_result = t_result + v
-    else:
-      t_result = t_result + v + ','
-      
-  s_result = ''
-  i = 0
   s_name = db.load_student_name()
-  for k, v in s_name:
-    i += 1
-    if i is len(s_name):
-      s_result = s_result + v
-    else:
-      s_result = s_result + v + ','
-      
-  Contents = [{'title': 'Teacher', 'display': t_result},
-                 {'title': 'Student', 'display': s_result}]
+  Contents = [{'title': 'Teacher', 'display': t_name},
+                 {'title': 'Student', 'display': s_name}]
   return render_template('home.html', contents=Contents, school='RBS')
 
 
-@app.route('/teacher')
-def list_teacher():
-  return 'hello' #jsonify(Contents)
-
+@app.route('/Teacher')
+def display_teacher():
+  return render_template('detail-info.html', contents='Teachers' )
+  
+@app.route('/Student')
+def display_student():
+  return render_template('detail-info.html', contents='Students' )
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
--- database.py
+++ database.py
@@ -3,13 +3,32 @@ import sqlite3
 conn = sqlite3.connect('rbs.db', check_same_thread=False)
 c = conn.cursor()
 
+def make_list(dic):
+  result = ''
+  i = 0
+  
+  for k, v in dic:
+    i += 1
+    if i is len(dic):
+      result = result + v
+    else:
+      result = result + v + ','
+  return result
 
 def load_teacher_name():
   result = c.execute('select "display", name from teacher')
   t_name = result.fetchall()
-  return t_name
+  result = make_list(t_name)
+  return result
 
 def load_student_name():
   result = c.execute('select "display", name from student')
   s_name = result.fetchall()
-  return s_name
+  result = make_list(s_name)
+  return result
+
+def load_teacher(id):
+  result = c.execute('select * from teacher where id = ?',(id)).fetchall()
+  print(type(result))
+
+load_teacher('1')
